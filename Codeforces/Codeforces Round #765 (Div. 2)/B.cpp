8b70f688a01efdf287fcbb8fe2778444967df9ea
Shreyas Pandey <shreyaspandey@Shreyass-MacBook-Air.local>
2022-01-12 22:26:29 +0530

    Correction B

--- Codeforces/Codeforces Round #765 (Div. 2)/B.cpp
+++ Codeforces/Codeforces Round #765 (Div. 2)/B.cpp
@@ -50,96 +50,30 @@ int main() {
     ISO();
     online_judge();
     ll t;
-    //cin >> t;
-    t = 1;
+    cin >> t;
     while (t--) {
-
         ll n;
         cin >> n;
-        ll l, k;
-        cin >> l >> k;
-        vector <ll> d(n);
-        for (ll i = 0; i < n; ++i) {
-            cin >> d[i];
-        }
         vector <ll> a(n);
         for (ll i = 0; i < n; ++i) {
             cin >> a[i];
         }
-        if (n == 1) {
-            cout << a[0]*l << "\n";
-            continue;
+        ll ans = -1;
+        vector <vector<ll>> f(2e5, vector<ll>());
+
+        for (ll i = 0; i < n; ++i) {
+            f[a[i]].push_back(i);
         }
-        set <vector<pair<ll, ll>>> speed;
-        vector<pair<ll, ll>> temp;
-        temp.push_back({0, 0});
-        temp.push_back({0, a[0]});
-        temp.push_back({d[1], a[1]});
-        speed.insert(temp);
-        for (ll i = 1; i < n - 1; ++i) {
-            vector<pair<ll, ll>> temp;
-            temp.push_back({d[i - 1], a[i - 1]});
-            temp.push_back({d[i], a[i]});
-            temp.push_back({d[i + 1], a[i + 1]});
-            speed.insert(temp);
-        }
-
-        temp.clear();
-
-        temp.push_back({d[n - 2], a[n - 2]});
-        temp.push_back({d[n - 1], a[n - 1]});
-        temp.push_back({l, 0});
-        speed.insert(temp);
-
-
-        while (k) {
-            ll total = 0;
-            for (auto x : speed) {
-                //cerr << x[1].second << "\n";
-                total += (x[2].first - x[1].first) * x[1].second;
-            }
-         //   cerr << total << "\n";
-            auto curr = speed.begin();
-            for (auto itr = speed.begin(); itr != speed.end(); ++itr) {
-                if (itr == speed.begin()) {
-                    continue;
+        for (auto x : f) {
+            sort(x.begin(), x.end());
+            if (x.size() > 0)
+                for (ll i = 0; i < x.size() - 1; ++i) {
+                    ll j = i + 1;
+                    ll x1 = x[i];
+                    ll x2 = x[j];
+                    ll len = x1 + (n - x2 - 1) + 1;
+                    ans = max(ans, len);
                 }
-                auto x = *itr;
-                ll val1 = (x[2].first - x[1].first) * x[1].second;
-                ll val2 = (x[2].first - x[1].first) * x[0].second;
-                ll new_total = total - val1 + val2;
-               // cerr << new_total << " " << total << " " << x[1].second << "\n";
-                if (new_total < total) {
-                    curr = itr;
-                }
-            }
-            if (curr != speed.begin()) {
-
-                auto prev = curr;
-                prev--;
-                auto nxt = curr;
-                nxt++;
-                auto prevval = *prev;
-                auto currval = *curr;
-                prevval[2] = currval[2];
-                if (nxt != speed.end()) {
-                    auto nextval = *nxt;
-                    nextval[0] = currval[0];
-                    speed.erase(nxt);
-                    speed.insert(nextval);
-                }
-               // cerr << currval[1].second << "\n";
-                speed.erase(curr);
-                speed.erase(prev);
-                speed.insert(prevval);
-            }
-
-            k--;
-        }
-        ll ans = 0;
-        for (auto x : speed) {
-            // cerr << x[1].first << "\n";
-            ans += (x[2].first - x[1].first) * x[1].second;
         }
         cout << ans << "\n";
 
