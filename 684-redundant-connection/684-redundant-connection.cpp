235fa647389b5b9de171864046746d8ede052e47
Shreyas Pandey <49951762+shreydevep@users.noreply.github.com>
2022-01-25 14:02:56 +0530

    Time: 7 ms (79.82%), Space: 9 MB (51.91%) - LeetHub

--- 684-redundant-connection/684-redundant-connection.cpp
+++ 684-redundant-connection/684-redundant-connection.cpp
@@ -1,39 +1,21 @@
 class Solution {
 public:
-    vector <int> par;
-    vector <int> sz;
-    void init(int n){
-        for(int i=0;i<=n;++i){
-            par.push_back(i);
-            sz.push_back(1);
-        }
-    }
-    
-    int find_set(int x){
-        if(x == par[x]) return par[x];
-            return par[x] = find_set(par[x]);
-    }
-    void union_set(int a,int b){
-        a = find_set(a);
-        b = find_set(b);
-        
-        if(a != b){
-            if(sz[a] < sz[b]){
-                swap(a,b);
-            }
-            par[b] = a;
-            sz[a] += sz[b];
-        }
-    }
-    
+   
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-       init(edges.size());
-        for(auto edge: edges){
-            if(find_set(edge[0]) == find_set(edge[1])){
-                return edge;
-            }
-            union_set(edge[0],edge[1]);
+       vector<int> p(edges.size()+1, 0);
+        for(int i = 0; i <= edges.size(); i++ )
+            p[i] = i;
+        
+        vector<int> res;
+        for(auto v : edges ){
+            int n1 = v[0], n2 = v[1];
+            while(n1 != p[n1]) n1 = p[n1];
+            while(n2 != p[n2]) n2 = p[n2];
+            if( n1 == n2 )
+                res = v;
+            else
+                p[n1] = n2;
         }
-        return edges.back();
+        return res;
     }
 };
