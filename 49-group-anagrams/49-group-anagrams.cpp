8e3a139acc6b3118c3b52c8ce761a075979879f7
Shreyas Pandey <49951762+shreydevep@users.noreply.github.com>
2022-01-25 16:01:33 +0530

    Time: 109 ms (11.39%), Space: 28.9 MB (8.00%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -1,11 +1,14 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string,vector<string>> hash;
+        map<vector<int>,vector<string>> hash;
+        
         for(auto x : strs){
-            string temp = x;
-            sort(temp.begin(),temp.end());
-            hash[temp].push_back(x);
+            vector<int> freq(26,0);
+            for(auto y : x){
+                freq[y-'a']++;
+            }
+            hash[freq].push_back(x);
         }
         vector<vector<string>> res;
          res.reserve(hash.size());
