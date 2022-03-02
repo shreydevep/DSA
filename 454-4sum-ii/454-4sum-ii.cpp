42bc97418ffdaf3cc7c9724db76187b56a6b8225
Shreyas Pandey <49951762+shreydevep@users.noreply.github.com>
2022-03-02 10:44:15 +0530

    Time: 290 ms (72.07%), Space: 27.7 MB (12.19%) - LeetHub

--- 454-4sum-ii/454-4sum-ii.cpp
+++ 454-4sum-ii/454-4sum-ii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    map<int,int> getPairSum(vector <int> &num1,vector <int> &num2){
-        map<int,int> mp;
+    unordered_map<int,int> getPairSum(vector <int> &num1,vector <int> &num2){
+        unordered_map<int,int> mp;
         
         for(int i=0;i<num1.size();++i){
             for(int j=0;j<num2.size();++j){
@@ -11,8 +11,8 @@ public:
         return mp;
     }
     int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
-        map<int,int> pair_sum_mp1 = getPairSum(nums1,nums2);
-        map<int,int> pair_sum_mp2 = getPairSum(nums3,nums4);
+        unordered_map<int,int> pair_sum_mp1 = getPairSum(nums1,nums2);
+        unordered_map<int,int> pair_sum_mp2 = getPairSum(nums3,nums4);
         int ans = 0;
         for(auto sum1 : pair_sum_mp1){
             if(pair_sum_mp2.find(-1*sum1.first) != pair_sum_mp2.end()){
