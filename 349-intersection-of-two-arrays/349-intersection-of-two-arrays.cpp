5ba6f26a23d7a4196f8cad082a8c38e2b8c1f81f
Shreyas Pandey <49951762+shreydevep@users.noreply.github.com>
2022-03-03 17:14:55 +0530

    Time: 3 ms (95.82%), Space: 10.2 MB (62.42%) - LeetHub

---
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        unordered_set <int> st;
+        for(auto x : nums1){
+            st.insert(x);
+        }
+       vector <int> ans;
+        for(auto x : nums2){
+            if(st.find(x) != st.end()){
+                ans.push_back(x);
+                st.erase(x);
+            }
+        }
+        return ans;
+    }
+};
