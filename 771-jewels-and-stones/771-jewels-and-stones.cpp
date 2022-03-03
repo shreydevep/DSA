ac49e27b90d8cf84b5acbc4f435b3407bd54a791
Shreyas Pandey <49951762+shreydevep@users.noreply.github.com>
2022-03-03 17:24:04 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (55.85%) - LeetHub

--- 771-jewels-and-stones/771-jewels-and-stones.cpp
+++ 771-jewels-and-stones/771-jewels-and-stones.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     int numJewelsInStones(string jewels, string stones) {
         set<char> mp;
-        for(auto x : jewels){
+        for(auto &x : jewels){
             mp.insert(x);
         }
          int ans = 0;
-        for(auto x : stones){
+        for(auto &x : stones){
             if(mp.find(x) != mp.end()) ans++;
         }
         
