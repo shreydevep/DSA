22a5ea78c06eb596a1a224a4067966eb5e322f50
Shreyas Pandey <49951762+shreydevep@users.noreply.github.com>
2022-03-04 16:21:42 +0530

    Time: 528 ms (30.72%), Space: 146.4 MB (22.49%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -1,26 +1,40 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        multiset<int> st;
-        priority_queue<int> pq;
-        vector<int> ans;
+        deque <int> q;
         int i=0;
-        
-        for(i=0;i<k;++i){
-            st.insert(nums[i]);
-            pq.push(nums[i]);
-        }
-        ans.push_back(pq.top());
+        vector <int> ans;
         int curr = 0;
-        for(int i=k;i<nums.size();++i){
-            st.erase(st.find(nums[curr]));
-            st.insert(nums[i]);
-            pq.push(nums[i]);
-            
-            while(!pq.empty() && st.find(pq.top()) == st.end()){
-                pq.pop();
+        for(i=0;i<k;++i){
+            if(!q.empty() && q.back() < nums[i]){
+                while(!q.empty()) q.pop_back();
+                q.push_back(nums[i]);
             }
-            ans.push_back(pq.top());
+            else{
+                while(!q.empty() && q.front() < nums[i]){
+                    q.pop_front();
+                }
+                q.push_front(nums[i]);
+            }
+            
+        }
+        
+        ans.push_back(q.back());
+        for(int i=k;i<nums.size();++i){
+            if(!q.empty() && q.back() == nums[curr]) q.pop_back();
+            if(!q.empty() && q.back() < nums[i]){
+                while(!q.empty()) {
+                        q.pop_back();
+                    }
+                q.push_back(nums[i]);
+            }
+            else{
+                while(!q.empty() && q.front() < nums[i]){
+                    q.pop_front();
+                }
+                q.push_front(nums[i]);
+            }
+            ans.push_back(q.back());
             curr++;
         }
         return ans;
