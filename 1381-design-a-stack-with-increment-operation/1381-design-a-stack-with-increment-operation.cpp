e2ada39edc5d1fd628c07866ca235d7714540594
Shreyas Pandey <49951762+shreydevep@users.noreply.github.com>
2022-03-08 13:55:01 +0530

    Time: 45 ms (61.79%), Space: 21.2 MB (18.93%) - LeetHub

--- 1381-design-a-stack-with-increment-operation/1381-design-a-stack-with-increment-operation.cpp
+++ 1381-design-a-stack-with-increment-operation/1381-design-a-stack-with-increment-operation.cpp
@@ -2,29 +2,34 @@
 class CustomStack {
 public:
     vector<int> st;
+    stack<int> s;
     int mxSize = 0;
     CustomStack(int maxSize) {
         mxSize = maxSize;
+        st.resize(maxSize+1);
+        st[0] = 0;
        
     }
     
     void push(int x) {
-        if(mxSize == st.size()) return;
-        st.push_back(x);
+        if(mxSize == s.size()) return;
+        s.push(x);
     }
     
     int pop() {
-        if(st.size() == 0) return -1;
-        int temp = st.back();
-        st.pop_back();
-        return temp;
+        if(s.size() == 0) return -1;
+            
+        int val = s.top() + st[s.size()];
+        
+        st[s.size()-1] += st[s.size()];
+        st[s.size()] = 0;
+        s.pop();
+        return (val);
        
     }
     
     void increment(int k, int val) {
-        for(int i=0;i< min((int)st.size(),k);++i){
-            st[i] += val;
-        }
+        st[min((int)s.size(),k)] += val;    
     }
 };
 
