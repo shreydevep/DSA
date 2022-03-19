7a92e53b9159f7850c8057772ead39a4d770c5a7
Shreyas Pandey <49951762+shreydevep@users.noreply.github.com>
2022-03-19 10:49:11 +0530

    Time: 200 ms (81.62%), Space: 79.5 MB (88.47%) - LeetHub

---
+++ 895-maximum-frequency-stack/895-maximum-frequency-stack.cpp
@@ -0,0 +1,46 @@
+#define pii pair<int,pair<int,int>>
+class FreqStack {
+public:
+    int clk;
+    
+    class Compare{
+        public:
+        bool operator()(const pii &a, const pii &b){
+            if((a.second).first == (b.second).first){  
+                        return (a.second).second < (b.second).second;
+            }
+            else{
+                return (a.second).first < (b.second).first;
+            }
+                        
+        }
+    };
+    priority_queue<pii,vector<pii>,Compare> pq;
+    map<int,int> mp;
+    FreqStack() {
+        
+        clk = 0;
+        
+    }
+    
+    void push(int val) {
+        clk++;
+        mp[val]++;
+        pq.push({val,{mp[val],clk}});
+    }
+    
+    int pop() {
+        int temp = (pq.top()).first;
+        mp[(pq.top()).first]--;
+        pq.pop();
+        return temp;
+    }
+};
+
+
+/**
+ * Your FreqStack object will be instantiated and called as such:
+ * FreqStack* obj = new FreqStack();
+ * obj->push(val);
+ * int param_2 = obj->pop();
+ */
