c87605da131fb2ced772f522b936aa9e73eb81d0
Shreyas Pandey <49951762+shreydevep@users.noreply.github.com>
2022-03-25 15:44:42 +0530

    Time: 434 ms (41.34%), Space: 9.8 MB (71.76%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/474-ones-and-zeroes.cpp
@@ -1,33 +1,21 @@
 class Solution {
 public:
-    pair<int,int> countOnesNzeros(string s){
-        int ones = 0;
-        int zeros = 0;
-        for(auto x : s){
-            if(x == '0') zeros++;
-            else ones++;
-        }
-        return {zeros,ones};
-    }
-    
-    int recurr(int i,int m,int n,vector<string> &strs,vector<vector<vector<int>>> &dp){
-        if(i == strs.size()){
-            return 0;
-        }
-        if(dp[i][m][n] != -1){
-            return dp[i][m][n];
-        }
-        auto p = countOnesNzeros(strs[i]);
-        if(p.first <= m && p.second <= n){
-            return dp[i][m][n] = max(recurr(i+1,m-p.first,n-p.second,strs,dp)+1,recurr(i+1,m,n,strs,dp));
-        }
-        else{
-            return dp[i][m][n] = recurr(i+1,m,n,strs,dp);
-        }
-        
-    }
     int findMaxForm(vector<string>& strs, int m, int n) {
-        vector<vector<vector<int>>> dp(strs.size(),vector<vector<int>>(m+1,vector<int>(n+1,-1)));
-        return recurr(0,m,n,strs,dp);
+        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
+        
+        for(auto &s : strs){
+            int zeros = 0;
+            int ones = 0;
+            for(auto &x : s){
+                if(x == '0') zeros++;
+                else ones++;
+            }
+            for(int i=m;i>=zeros;--i){
+                for(int j=n;j>=ones;--j){
+                    dp[i][j] = max(dp[i][j],dp[i-zeros][j-ones] + 1);
+                }
+            }
+        }
+        return dp[m][n];
     }
 };
