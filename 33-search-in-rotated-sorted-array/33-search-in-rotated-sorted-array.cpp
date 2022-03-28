df8fa025f7653f89483f9247ba9e37c17c523628
Shreyas Pandey <49951762+shreydevep@users.noreply.github.com>
2022-03-28 20:09:46 +0530

    Time: 11 ms (15.26%), Space: 11.2 MB (29.80%) - LeetHub

---
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l = 0;
+        int r = nums.size() - 1;
+        int mid = l + (r-l)/2;        
+        while(l < r){
+            mid = l + (r-l)/2;
+            if(nums[mid] >= nums[r]){
+                l = mid + 1;
+            }
+            else{
+                r = mid;
+            }
+        }
+        int pivot = max(0,r-1);
+        int itr1 = -1;
+        int itr2 = -1;
+        if(binary_search(nums.begin(),nums.begin()+pivot+1,target)){
+             itr1 = lower_bound(nums.begin(),nums.begin()+pivot+1,target) - nums.begin();
+        }
+        if(binary_search(nums.begin()+pivot+1,nums.end(),target)){
+             itr2 = lower_bound(nums.begin()+pivot+1,nums.end(),target) - nums.begin();
+        }
+       
+       
+        
+        if(itr1 != -1){
+            return itr1;
+        }
+        return itr2;
+    }
+};
