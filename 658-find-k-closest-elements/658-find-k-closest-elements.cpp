84d5c0fb35ec60511a10c5b44177e7bb56df65c1
Shreyas Pandey <49951762+shreydevep@users.noreply.github.com>
2022-05-07 08:37:38 +0530

    Time: 65 ms (55.03%), Space: 31.1 MB (89.86%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -3,19 +3,21 @@ public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
         //So we need to get the window of size k such that the abs difference 
         //of elements is equal to k
-        int L = 0;
-        int R = arr.size() - 1;
-        while(R-L >= k){
-            if(L >= 0 && R < arr.size() && abs(arr[L] - x) < abs(arr[R] - x)){
-                R--;
+        
+        int R = lower_bound(arr.begin(),arr.end(),x) - arr.begin();
+        int L = R-1;
+        while(k--){
+            if(R >= arr.size() || (L>=0 && abs(arr[L] - x) < abs(arr[R] - x))){
+                L--;
             }
-            else if((abs(arr[L] - x) == abs(arr[R] - x) && arr[L] < arr[R])){
-                R--;
+            else if(L>= 0 && abs(arr[L] - x) == abs(arr[R] - x) && arr[L] < arr[R]){
+                L--;
             }
             else{
-                L++;
+                R++;
             }
         }
-        return vector<int> (begin(arr) + L, begin(arr) + R + 1);
+        
+        return vector<int> (begin(arr) + L + 1, begin(arr) + R);
     }
 };
