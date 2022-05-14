01ca110bca195d5fc834aab161d0e516904602fb
Shreyas Pandey <49951762+shreydevep@users.noreply.github.com>
2022-05-14 21:36:29 +0530

    Time: 10 ms (72.86%), Space: 10.1 MB (27.76%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -3,20 +3,24 @@ public:
     int minPathSum(vector<vector<int>>& grid) {
         vector<vector<int>> dp(grid.size()+1,vector<int>(grid[0].size()+1,0));
         
-        dp[0][0] = grid[0][0];
         
-        for(int i=1;i<grid.size();++i){
-            dp[i][0] = dp[i-1][0] + grid[i][0];
+        vector<int> prev(grid[0].size(),0);
+        
+        vector<int> curr(grid[0].size(),0);
+        prev[0] = grid[0][0];
+        
+        for(int i=1;i<grid[0].size();++i){
+            prev[i] = prev[i-1] + grid[0][i];
         }
-        for(int j=1;j<grid[0].size();++j){
-            dp[0][j] = dp[0][j-1] + grid[0][j];
-        }
+        curr[0] = grid[0][0];
         for(int i=1;i<grid.size();++i){
+            curr[0] += grid[i][0];
             for(int j=1;j<grid[i].size();++j){
-                dp[i][j] = min(dp[i-1][j],dp[i][j-1]) + grid[i][j];
+                curr[j] = min(prev[j],curr[j-1]) + grid[i][j];
             }
+            prev = curr;
         }
-        return dp[grid.size()-1][grid[0].size()-1];
+        return prev.back();
         
     }
 };
