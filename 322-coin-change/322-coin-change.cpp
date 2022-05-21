29012e2495095c57a295d268a64e9eb83f201a4c
Shreyas Pandey <49951762+shreydevep@users.noreply.github.com>
2022-05-21 18:00:57 +0530

    Time: 203 ms (16.39%), Space: 49.1 MB (13.54%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -2,36 +2,28 @@ class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
         
-        int dp[coins.size() + 1][amount + 1];
+        
+        vector<vector<int>> dp(coins.size()+1,vector<int>(amount+1,10000002));
         
         for(int i=0;i<=coins.size();++i){
-            for(int j=0;j<=amount;++j){
-                dp[i][j] = 10002;
-            }
-        }
-        
-        for(int i=1;i<=coins.size();++i){
             dp[i][0] = 0;
+        }
+        for(int j=0;j<=amount;++j){
+            if(j % coins[0] == 0)
+                dp[1][j] = (j/coins[0]);
+            else dp[1][j] = 10000002;
         }
         
         for(int i=1;i<=amount;++i){
-            if(i % coins[0] == 0){
-                dp[1][i] = i/coins[0];
+            for(int j=2;j<=coins.size();++j){
+                if(coins[j-1] <= i)
+                    dp[j][i] = min(dp[j][i-coins[j-1]] + 1,dp[j-1][i]);
+                else 
+                    dp[j][i] = dp[j-1][i];
             }
         }
         
-         for(int i=1;i<=coins.size();++i){
-            for(int j=1;j<=amount;++j){
-           
-                if(coins[i-1] <= j){
-                    dp[i][j] = min(dp[i-1][j],dp[i][j-coins[i-1]] + 1);
-                }
-                else{
-                    dp[i][j] = dp[i-1][j];
-                }
-            }
-        }
-        if(dp[coins.size()][amount] == 10002) return -1;
+        if(amount != 0 && dp[coins.size()][amount] == 10000002) return -1;
         return dp[coins.size()][amount];
     }
 };
