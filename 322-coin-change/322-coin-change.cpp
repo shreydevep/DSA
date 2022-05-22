df0b3d1be2ca410b95ee2b82348b5bfa3408e562
Shreyas Pandey <49951762+shreydevep@users.noreply.github.com>
2022-05-22 10:25:51 +0530

    Time: 110 ms (57.34%), Space: 18.5 MB (25.48%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -2,28 +2,30 @@ class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
         
-        
-        vector<vector<int>> dp(coins.size()+1,vector<int>(amount+1,10000002));
-        
-        for(int i=0;i<=coins.size();++i){
-            dp[i][0] = 0;
-        }
+        vector<int> prev(amount+1,10000002);
+        vector<int> curr(amount+1,10000002);
+
         for(int j=0;j<=amount;++j){
             if(j % coins[0] == 0)
-                dp[1][j] = (j/coins[0]);
-            else dp[1][j] = 10000002;
+                prev[j] = (j/coins[0]);
         }
-        
-        for(int i=1;i<=amount;++i){
-            for(int j=2;j<=coins.size();++j){
-                if(coins[j-1] <= i)
-                    dp[j][i] = min(dp[j][i-coins[j-1]] + 1,dp[j-1][i]);
-                else 
-                    dp[j][i] = dp[j-1][i];
+        curr = prev;
+       
+        for(int i=2;i<=coins.size();++i){
+            curr[0] = 0;
+            for(int j=0;j<=amount;++j){
+                if(coins[i-1] <= j){
+                    curr[j] = min(curr[j-coins[i-1]] + 1,prev[j]);
+                }
+                else{
+                    curr[j] = prev[j];
+                }
+                
             }
+            
+            prev = curr;
         }
-        
-        if(amount != 0 && dp[coins.size()][amount] == 10000002) return -1;
-        return dp[coins.size()][amount];
+        if(prev[amount] == 10000002 && amount > 0) return -1;
+        return prev[amount];
     }
 };
