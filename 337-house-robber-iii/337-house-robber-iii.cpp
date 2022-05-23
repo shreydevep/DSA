66e25fa98e459039d1658122e67458a5509314d7
Shreyas Pandey <49951762+shreydevep@users.noreply.github.com>
2022-05-24 01:18:49 +0530

    Time: 21 ms (60.01%), Space: 22.1 MB (91.75%) - LeetHub

--- 337-house-robber-iii/337-house-robber-iii.cpp
+++ 337-house-robber-iii/337-house-robber-iii.cpp
@@ -11,27 +11,18 @@
  */
 class Solution {
 public:
-    int solve(TreeNode* node,bool canBreak,map<pair<TreeNode*,int>,int> &dp){
-        if(node == NULL) return 0;
-        if(dp.find({node,canBreak}) != dp.end()) return dp[{node,canBreak}];
-        
-        int breakCurr = node->val + solve(node->left,0,dp) + solve(node->right,0,dp);
-        
-        int notBreakCurr = solve(node->left,1,dp) + solve(node->right,1,dp);
-        
-        if(canBreak){
-            
-            return dp[{node,canBreak}] = max(breakCurr,notBreakCurr);
-        }
-        else{
-            
-            return dp[{node,canBreak}] = notBreakCurr;
-            
-        }
+    
+    pair<int, int> dfs(TreeNode* root) {
+        if(!root) return {0, 0};
+        auto [leftDontRob, leftRob]   = dfs(root -> left);
+        auto [rightDontRob, rightRob] = dfs(root -> right);
+        return {
+            max(leftDontRob, leftRob) + max(rightDontRob, rightRob),
+            root -> val + leftDontRob + rightDontRob
+        };
     }
     int rob(TreeNode* root) {
-        if(root == NULL) return 0;
-        map<pair<TreeNode*,int>,int> dp;
-        return max((solve(root->left,1,dp) + solve(root->right,1,dp)),(solve(root->left,0,dp) + solve(root->right,0,dp)) + root->val);   
+        auto ans = dfs(root);
+        return max(ans.first,ans.second);
     }
 };
