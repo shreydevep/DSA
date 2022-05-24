ca77426c6bff9db627492b58beef235ad37da25f
Shreyas Pandey <49951762+shreydevep@users.noreply.github.com>
2022-05-24 13:22:07 +0530

    Time: 4 ms (39.02%), Space: 8.4 MB (45.61%) - LeetHub

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.cpp
@@ -11,45 +11,15 @@
  */
 class Solution {
 public:
-    TreeNode* getRightMostNode(TreeNode* leftNode,TreeNode* curr){
-        //Curr Subtree LeftNode Right Most Node
-        while(leftNode->right != NULL && leftNode->right != curr){
-            leftNode = leftNode->right;
-        }
-        return leftNode;
-    }
-    vector<int> morrisPreorder(TreeNode* root){
-        TreeNode* curr = root;
-        vector<int> ans;
-        while(curr != NULL){
-            TreeNode* leftNode = curr->left;
-             
-            if(leftNode == NULL){
-                //Process Current
-                ans.push_back(curr->val);
-                curr = curr->right;
-            }else{
-                TreeNode* rightMostNode = getRightMostNode(leftNode,curr);
-                if(rightMostNode->right == NULL){
-                    //Create Thread
-                    ans.push_back(curr->val);
-                    rightMostNode->right = curr;
-                    curr = curr->left;
-                }
-                else{
-                    //Break Thread
-                    rightMostNode->right = NULL;
-                    //ans.push_back(curr->val);
-                    curr = curr->right;
-                    
-                }
-                
-            }
-             
-        }
-        return ans;
+    vector<int> preorderEle;
+    void preorder(TreeNode* root){
+        if(!root) return;
+        preorderEle.push_back(root->val);
+        preorder(root->left);
+        preorder(root->right);
     }
     vector<int> preorderTraversal(TreeNode* root) {
-        return morrisPreorder(root);
+        preorder(root);
+        return preorderEle;
     }
 };
