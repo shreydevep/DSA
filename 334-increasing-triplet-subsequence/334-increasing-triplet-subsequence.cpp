1b476d97cb2aea0ed2e1751363c1e3993d14b65e
Shreyas Pandey <49951762+shreydevep@users.noreply.github.com>
2022-05-25 16:08:31 +0530

    Time: 91 ms (52.37%), Space: 61.6 MB (70.77%) - LeetHub

--- 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.cpp
+++ 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.cpp
@@ -16,6 +16,22 @@ public:
         return (len > 2) ?(true) : false;
     }
     bool increasingTriplet(vector<int>& nums) {
-       return lis(nums);
+        int one = INT_MAX;
+        int two = INT_MAX;
+        for(auto &x : nums){
+            if(x < one){
+                one = x;
+                continue;
+            }
+            if(x < two && x > one){
+                two = x;
+                continue;
+            }
+            if(one < two && two < x){
+                return true;
+            }
+            
+        }
+        return false;
     }
 };
