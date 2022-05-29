8db23917994ffadd8cba57319ee88211d048eff4
Shreyas Pandey <49951762+shreydevep@users.noreply.github.com>
2022-05-29 14:26:17 +0530

    Time: 1611 ms (5.05%), Space: 18.1 MB (8.42%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
     //map<pair<pair<int,int>,pair<int,int>>,int> dp;
-    
-    int mcm(int i,int j,int cutMinInd,int cutMaxInd,vector<int> &cuts,vector<vector<int>> &dp){
+    map<pair<int,int>,int> dp;
+    int mcm(int i,int j,int cutMinInd,int cutMaxInd,vector<int> &cuts){
         
         if(cutMinInd > cutMaxInd) return 0;
         if(i >= j) return 1e8;
         //if(dp.find({{i,j},{cutMinInd,cutMaxInd}}) != dp.end()) return dp[{{i,j},{cutMinInd,cutMaxInd}}];
-        if(dp[cutMinInd][cutMaxInd] != -1) return dp[cutMinInd][cutMaxInd]; 
-        
+        if(dp.find({cutMinInd,cutMaxInd}) != dp.end()) return dp[{cutMinInd,cutMaxInd}]; 
         int temp = 1e8;
         for(int k=cutMinInd;k<=cutMaxInd;++k){
-            int val = mcm(i,cuts[k],cutMinInd,k-1,cuts,dp) + mcm(cuts[k],j,k+1,cutMaxInd,cuts,dp) + (j-i);
+            int val = mcm(i,cuts[k],cutMinInd,k-1,cuts) + mcm(cuts[k],j,k+1,cutMaxInd,cuts) + (j-i);
              temp = min(temp,val);
         }
         //return dp[{{i,j},{cutMinInd,cutMaxInd}}] = temp;
-        return dp[cutMinInd][cutMaxInd] = temp;
+        return dp[{cutMinInd,cutMaxInd}] = temp;
     }
     int minCost(int n, vector<int>& cuts) {
         sort(cuts.begin(),cuts.end());
-        vector<vector<int>> dp(cuts.size()+1,vector<int>(cuts.size()+1,-1));
         
-        return mcm(0,n,0,cuts.size()-1,cuts,dp);
+        
+        return mcm(0,n,0,cuts.size()-1,cuts);
     }
 };
