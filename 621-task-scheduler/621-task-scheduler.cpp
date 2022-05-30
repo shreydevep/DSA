7b15284042f533c86bf81fad55d76abc8ba9b546
Shreyas Pandey <49951762+shreydevep@users.noreply.github.com>
2022-05-30 22:09:40 +0530

    Time: 766 ms (5.03%), Space: 58.8 MB (5.03%) - LeetHub

---
+++ 621-task-scheduler/621-task-scheduler.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+       
+        
+        unordered_map<char,int> freq;
+        int cnt = tasks.size();
+        for(auto &task : tasks){
+            freq[task]++;
+        }
+        priority_queue<pair<int,char>> pq;
+        map<int,pair<int,char>> reschedule;
+        for(auto &x : freq){
+            pq.push({x.second,x.first});
+        }
+        int timer = 0;
+        
+        while(cnt > 0 && timer < 1e5){
+            if(reschedule.find(timer) != reschedule.end()){
+               
+                pq.push(reschedule[timer]);
+            }
+            if(pq.empty() && reschedule.find(timer) == reschedule.end()){
+                auto itr = reschedule.lower_bound(timer);
+                timer++;
+                continue;
+            }
+            
+            auto val = pq.top();
+            pq.pop();
+            cnt--;
+            timer++;
+            
+            val = {val.first-1,val.second};
+            
+            if(val.first > 0){
+                reschedule[timer+n] = val;
+            }
+        }
+        return timer;
+        
+        
+    }
+};
