2bfbbd9cdd5c52fb79bedac9e873cf2bcd025d00
Shreyas Pandey <49951762+shreydevep@users.noreply.github.com>
2022-06-01 23:00:22 +0530

    Time: 95 ms (11.29%), Space: 30.2 MB (16.56%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -11,8 +11,23 @@ public:
             
         return dp[i][j] = take + nottake;
     }
+    int iterative(string s,string t,vector<vector<double>> &dp){
+        for(int i=0;i<=s.length();++i) dp[i][t.length()] = 1;
+        
+        for(int i=s.length()-1;i>=0;--i){
+            for(int j=t.length()-1;j>=0;--j){
+                if(s[i] == t[j]){
+                    dp[i][j] = (dp[i+1][j+1] + dp[i+1][j]);
+                }
+                else{
+                    dp[i][j] = dp[i+1][j];
+                }
+            }
+        }
+        return dp[0][0];
+    }
     int numDistinct(string s, string t) {
-        vector<vector<int>> dp(s.length(),vector<int>(t.length(),-1));
-        return recurr(0,0,s,t,dp);
+        vector<vector<double>> dp(s.length()+1,vector<double>(t.length()+1,0));
+        return iterative(s,t,dp);
     }
 };
