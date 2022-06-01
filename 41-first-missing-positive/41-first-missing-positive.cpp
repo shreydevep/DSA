24cb1b3a18ddde165e30f77d32d370af238646d9
Shreyas Pandey <49951762+shreydevep@users.noreply.github.com>
2022-06-01 15:25:13 +0530

    Time: 208 ms (59.18%), Space: 83.1 MB (41.51%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.cpp
+++ 41-first-missing-positive/41-first-missing-positive.cpp
@@ -1,40 +1,19 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        int curr_neg = 0;
-        while(curr_neg < nums.size() && nums[curr_neg] > 0){
-            curr_neg++;
-        }
-        int curr = nums.size() - 1;
-        while(curr > curr_neg){
-            if(nums[curr] <= 0){
-                curr--;
-                continue;
-            }
-            swap(nums[curr],nums[curr_neg]);
-            while(curr_neg < nums.size() && nums[curr_neg] > 0){
-                curr_neg++;
-            }
-            curr--;   
-        }
-        int cntPos = 0;
-        int sz = 0;
+        int ans = nums.size();
         for(int i=0;i<nums.size();++i){
-            if(nums[i] > 0){
-                cntPos++;
-                sz = i+1;
+           
+            while(nums[i] < nums.size() && nums[i] >= 0 && nums[i] != i && nums[nums[i]] != nums[i]){
+                swap(nums[i],nums[nums[i]]);
             }
         }
-        for(int i=0;i<sz;++i){
-            if(abs(nums[i]) <= cntPos && nums[abs(nums[i]) - 1] > 0)
-                nums[abs(nums[i]) - 1] *= -1;
+        
+        for(int i=0;i<nums.size();++i){
+            if(nums[i] == ans) ans++;
+            if(i == 0 or i == nums[i]) continue;
+            return i;
         }
-        int res = cntPos+1;
-        for(int i=0;i<sz;++i){
-            if(nums[i] > 0){
-                res = min(res,i+1);
-            }
-        }
-        return res;
+        return ans;
     }
 };
