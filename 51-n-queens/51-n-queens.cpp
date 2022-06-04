dc0fde44204c92ad7ee62fd5ca0ca09290ccd747
Shreyas Pandey <49951762+shreydevep@users.noreply.github.com>
2022-06-04 21:37:29 +0530

    Time: 14 ms (40.55%), Space: 12.1 MB (23.87%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,26 +1,13 @@
 class Solution {
 public:
+    vector<bool> row_hash;
+    vector<bool> diagonal_forward_hash;
+    vector<bool> diagonal_backward_hash;
     bool isAttacked(int x,int y,vector<string> &queensPos){
-        int i=x;
-        int j=y;
-        while(i >= 0 && j>= 0){
-            if(queensPos[i][j] == 'Q') return true;
-            i--;
-            j--;
-        }
-        i=x;
-        j=y;
-         while(j>= 0){
-            if(queensPos[i][j] == 'Q') return true;
-            j--;
-        }
-        i=x;
-        j=y;
-         while(i < queensPos.size() && j >=0){
-            if(queensPos[i][j] == 'Q') return true;
-            i++;
-            j--;
-        }
+        if(row_hash[x]) return true;
+        if(diagonal_forward_hash[x+y]) return true;
+        if(diagonal_backward_hash[queensPos.size()-1 + (y-x)]) return true;
+        
         
         return false;
         
@@ -29,21 +16,29 @@ public:
     void recurr(int col,vector<string> queensPos){
         
         if(col == queensPos[0].size()){
-            
-                ans.push_back(queensPos);
-            
+            ans.push_back(queensPos);
             return;
         }
         
         for(int row=0;row<queensPos.size();++row){
             if(isAttacked(row,col,queensPos)) continue;
             queensPos[row][col] = 'Q';
+            row_hash[row] = true;
+            diagonal_forward_hash[row+col] = true;
+            diagonal_backward_hash[queensPos.size()-1 + (col-row)] = true;
             recurr(col+1,queensPos);
+            row_hash[row] = false;
+            diagonal_forward_hash[row+col] = false;
+            diagonal_backward_hash[queensPos.size()-1 + (col-row)] = false;
+            
             queensPos[row][col] = '.';
         }
     }
     vector<vector<string>> solveNQueens(int n) {
         vector<string> queensPos(n);
+        row_hash.resize(n,0);
+        diagonal_forward_hash.resize(2*n,0);
+        diagonal_backward_hash.resize(2*n,0);
         for(int i=0;i<n;++i){
             for(int j=0;j<n;++j){
                 queensPos[i].push_back('.');
