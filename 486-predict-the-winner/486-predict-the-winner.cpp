0a5b572a29cdc77be1d7ff1b9b2c9216fd69a4cc
Shreyas Pandey <49951762+shreydevep@users.noreply.github.com>
2022-06-22 23:18:07 +0530

    Time: 505 ms (5.08%), Space: 7.5 MB (65.68%) - LeetHub

---
+++ 486-predict-the-winner/486-predict-the-winner.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int dp[21][21] = {-1};
+    int recurr(int i,int j,bool chance,vector<int> &nums){
+        if(i > j) return 0;
+        //if(dp[i][j] != -1) return dp[i][j];
+        int takeFront = recurr(i+1,j,!chance,nums);
+        int takeBack = recurr(i,j-1,!chance,nums);
+        
+        int ans = max(takeFront + nums[i],takeBack + nums[j]);
+        return  (chance == 0) ? ans : min(takeFront,takeBack);
+    }
+    bool PredictTheWinner(vector<int>& nums) {
+        int sum = 0;
+        for(auto x : nums) sum += x;
+        
+        int ans = recurr(0,nums.size()-1,0,nums);
+        //cout << ans <<" "<< (sum - ans) <<"\n";
+        if((sum-ans) > ans) return 0;
+        return 1;
+    }
+};
