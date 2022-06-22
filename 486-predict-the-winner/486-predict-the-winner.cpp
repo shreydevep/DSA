79ff28a58940c5ee9fa79e79dbb8b9251bbfee2a
Shreyas Pandey <49951762+shreydevep@users.noreply.github.com>
2022-06-22 23:27:13 +0530

    Time: 3 ms (74.88%), Space: 7.7 MB (24.22%) - LeetHub

--- 486-predict-the-winner/486-predict-the-winner.cpp
+++ 486-predict-the-winner/486-predict-the-winner.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
     vector<vector<int>> dp;
-    int recurr(int i,int j,vector<int> &nums){
-        
-        if(i == j) return nums[i];
-        
+    int recurr(int i,int j,bool chance,vector<int> &nums){
+        if(i > j) return 0;
         if(dp[i][j] != -1) return dp[i][j];
-        int takeFront = nums[i] - recurr(i+1,j,nums);
-        int takeBack = nums[j] - recurr(i,j-1,nums);
+        int takeFront = recurr(i+1,j,!chance,nums);
+        int takeBack = recurr(i,j-1,!chance,nums);
         
-        int ans = max(takeFront,takeBack);
-        return  dp[i][j] = ans;
+        int ans = max(takeFront + nums[i],takeBack + nums[j]);
+        return  dp[i][j] = (chance == 0) ? ans : min(takeFront,takeBack);
     }
     bool PredictTheWinner(vector<int>& nums) {
         int sum = 0;
-        dp.resize(nums.size(),vector<int>(nums.size(),-1));
         for(auto x : nums) sum += x;
+        dp.resize(nums.size(),vector<int>(nums.size(),-1));
         
-        int ans = recurr(0,nums.size()-1,nums);
+        int ans = recurr(0,nums.size()-1,0,nums);
         
-        return (ans >= 0);
-        
+        if((sum-ans) > ans) return 0;
+        return 1;
     }
 };
