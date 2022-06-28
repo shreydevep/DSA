2add30ed994f628693e71173f1f2d4b5c66b8ada
Shreyas Pandey <49951762+shreydevep@users.noreply.github.com>
2022-06-28 18:21:39 +0530

    Time: 1866 ms (5.13%), Space: 18 MB (16.12%) - LeetHub

---
+++ 1915-number-of-wonderful-substrings/1915-number-of-wonderful-substrings.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int countSetBits(int n)
+    {
+    // base case
+    if (n == 0)
+        return 0;
+    else
+        // if last bit set add 1 else add 0
+        return (n & 1) + countSetBits(n >> 1);
+    }
+    long long wonderfulSubstrings(string word) {
+        map<int,int> hash;
+        int prev = 0;
+        long long ans = 0;
+        hash[0] = 1;
+        
+        for(auto w : word){
+            int val = (w - 'a');
+            prev ^= (1 << val);
+            ans += hash[prev];
+            for(int i=0;i<10;++i){
+                ans += (hash[prev^(1 << i)]);
+            }
+            
+            hash[prev]++;
+            
+        }
+        return ans;
+    }
+};
+
+    
+    
