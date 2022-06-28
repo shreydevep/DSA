07fc52db638d847b20e68ad30a6828023e1ae16c
Shreyas Pandey <49951762+shreydevep@users.noreply.github.com>
2022-06-28 22:35:34 +0530

    Time: 583 ms (40.68%), Space: 192.1 MB (35.31%) - LeetHub

--- 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -12,31 +12,27 @@
 class Solution {
 public:
     int ans;
-    int countSetBits(int n)
-    {
-    // base case
-        if (n == 0)
-            return 0;
-        else
-            return 1 + countSetBits(n & (n - 1));
-    }
-    bool check(int mask){
-        if(countSetBits(mask) == 0 or countSetBits(mask) == 1) return true;
+    bool check(bitset<9> &mask){
+        if(mask.count() == 0 or mask.count() == 1) return true;
         return false;
     }
-    void recurr(TreeNode* root,int mask){
+    void recurr(TreeNode* root,bitset<9> &mask){
         
         if(root == NULL) return;
-        mask ^= (1 << (root->val-1));
+        mask.set(root->val-1,mask[root->val-1]^(1));
         if(root->left == NULL && root->right == NULL){
             ans += check(mask);
+            mask.set(root->val-1,mask[root->val-1]^(1));
             return;
         }
+        
         recurr(root->left,mask);
         recurr(root->right,mask);
+        mask.set(root->val-1,mask[root->val-1]^(1));
+        
     }
     int pseudoPalindromicPaths (TreeNode* root) {
-        int mask = 0;
+        bitset<9> mask(0);
         recurr(root,mask);
         return ans;
     }
