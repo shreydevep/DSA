376799995b0737a62682ae616abf47e8b349316f
Shreyas Pandey <49951762+shreydevep@users.noreply.github.com>
2022-06-28 18:29:18 +0530

    Time: 185 ms (63.00%), Space: 14.8 MB (69.23%) - LeetHub

--- 1915-number-of-wonderful-substrings/1915-number-of-wonderful-substrings.cpp
+++ 1915-number-of-wonderful-substrings/1915-number-of-wonderful-substrings.cpp
@@ -1,14 +1,6 @@
 class Solution {
 public:
-    int countSetBits(int n)
-    {
-    // base case
-    if (n == 0)
-        return 0;
-    else
-        // if last bit set add 1 else add 0
-        return (n & 1) + countSetBits(n >> 1);
-    }
+    
     long long wonderfulSubstrings(string word) {
         vector<int> hash(1024,0);
         int prev = 0;
