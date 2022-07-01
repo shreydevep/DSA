004842a2ba4634ae2c372d0cebe7a85e795c2625
Shreyas Pandey <49951762+shreydevep@users.noreply.github.com>
2022-07-01 11:29:57 +0530

    Time: 9 ms (73.55%), Space: 11.3 MB (52.65%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
    vector<vector<int>> results;
-    void recurr(int i,int target,vector<int> &candidates,vector<int> posCad){
+    void recurr(int i,int target,vector<int> &candidates,vector<int> &posCad){
         if(target == 0) {
                 reverse(posCad.begin(),posCad.end());
                 results.push_back(posCad);
+                reverse(posCad.begin(),posCad.end());
                 return;
         }
         if(target < 0 or i < 0) return;
-        
         
         posCad.push_back(candidates[i]);
         recurr(i,target-candidates[i],candidates,posCad);
@@ -21,3 +21,9 @@ public:
         return results;
     }
 };
+/**
+Here the approach goes as follows:
+1-> Unique numbers so either we choose a number or we don't until we have now choice just to leave the number;
+2-> No sorting is required as for every number we are exporing the 2 states;
+
+**/
