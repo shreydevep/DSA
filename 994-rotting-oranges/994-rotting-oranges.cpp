6c642497af1d2b038c9635f556e8647c6a2a4fa0
Shreyas Pandey <49951762+shreydevep@users.noreply.github.com>
2022-07-07 21:54:09 +0530

    Time: 28 ms (5.87%), Space: 15.7 MB (3.32%) - LeetHub

---
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    vector<vector<int>> dist;
+    const int inf = 1e5;
+    bool check(int x,int y,vector<vector<int>> &grid){
+        if(x < 0 || y < 0 || x >= grid.size() || y >= grid[0].size()) return 0;
+        return 1;
+    }
+    vector<int> dx = {-1,0,0,1};
+    vector<int> dy = {0,-1,1,0};
+    void bfs(pair<int,int> node,vector<vector<int>> &grid){
+        queue<pair<int,int>> q;
+        q.push(node);
+        while(!q.empty()){
+            
+            int sz = q.size();
+            for(int i=0;i<sz;++i){
+                auto [x,y] = q.front();
+                q.pop();
+                for(int mv=0;mv<4;++mv){
+                    if(check(x+dx[mv],y+dy[mv],grid)&& grid[x+dx[mv]][y+dy[mv]] != 0 && dist[x][y] + 1 < dist[x+dx[mv]][y+dy[mv]]){
+                        dist[x+dx[mv]][y+dy[mv]] = dist[x][y] + 1;
+                        q.push({x+dx[mv],y+dy[mv]});
+                    }
+                }
+            }
+            
+        }
+    }
+    int orangesRotting(vector<vector<int>>& grid) {
+        dist.resize(grid.size(),vector<int>(grid[0].size(),inf));
+        for(int i=0;i<grid.size();++i){
+            for(int j=0;j<grid[0].size();++j){
+                if(grid[i][j] == 2){
+                    dist[i][j] = 0;
+                    bfs({i,j},grid);
+                }
+            }
+        }
+        int ans = 0;
+        for(int i=0;i<grid.size();++i){
+            for(int j=0;j<grid[0].size();++j){
+                if(grid[i][j] != 0)
+                    ans = max(ans,dist[i][j]);
+            }
+        }
+        if(ans == inf) return -1;
+        return ans;
+        
+    }
+};
