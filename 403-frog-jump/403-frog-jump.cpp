7d40efa17f9c32dfbee3eab6cf84866f699f7bc4
Shreyas Pandey <49951762+shreydevep@users.noreply.github.com>
2022-07-08 15:25:42 +0530

    Time: 239 ms (58.58%), Space: 232.9 MB (14.03%) - LeetHub

--- 403-frog-jump/403-frog-jump.cpp
+++ 403-frog-jump/403-frog-jump.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-    map<pair<int,int>,int> dp;
+    vector<vector<int>> dp;
     bool recurr(int i,int j,vector<int>& stones){
         
         if(i == stones.size()-1) return true;
         if(i >= stones.size()) return false;
         int lastJump = stones[i]-stones[j];
-        if(dp.find({i,j}) != dp.end()) return dp[{i,j}];
+        if(dp[i][j] != -1) return dp[i][j];
         auto jump = lower_bound(stones.begin()+i+1,stones.end(),lastJump+stones[i]);
         
         auto jump_plus = lower_bound(stones.begin()+i+1,stones.end(),lastJump+1 + stones[i]);
@@ -23,9 +23,10 @@ public:
         if(jump_min < stones.end() && (*jump_min - stones[i]) == lastJump - 1){
              ans2 = recurr(jump_min-stones.begin(),i,stones);
         }
-        return dp[{i,j}] = (ans1|ans2|ans3);
+        return dp[i][j] = (ans1|ans2|ans3);
     }
     bool canCross(vector<int>& stones) {
+        dp.resize(stones.size(),vector<int>(stones.size(),-1));
         return recurr(0,0,stones);
     }
 };
