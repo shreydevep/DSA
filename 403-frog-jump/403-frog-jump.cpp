648871b3c4319c41d3a11627207c6297917dce88
Shreyas Pandey <49951762+shreydevep@users.noreply.github.com>
2022-07-08 15:23:10 +0530

    Time: 690 ms (11.64%), Space: 47.6 MB (51.12%) - LeetHub

--- 403-frog-jump/403-frog-jump.cpp
+++ 403-frog-jump/403-frog-jump.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     map<pair<int,int>,int> dp;
-    bool recurr(int i,int lastJump,vector<int>& stones){
+    bool recurr(int i,int j,vector<int>& stones){
         
         if(i == stones.size()-1) return true;
         if(i >= stones.size()) return false;
-       
-        if(dp.find({i,lastJump}) != dp.end()) return dp[{i,lastJump}];
+        int lastJump = stones[i]-stones[j];
+        if(dp.find({i,j}) != dp.end()) return dp[{i,j}];
         auto jump = lower_bound(stones.begin()+i+1,stones.end(),lastJump+stones[i]);
         
         auto jump_plus = lower_bound(stones.begin()+i+1,stones.end(),lastJump+1 + stones[i]);
@@ -14,16 +14,16 @@ public:
         
         bool ans1 = false,ans2 = false,ans3 = false;
         if(jump < stones.end() && (*jump - stones[i]) == lastJump){
-            ans3 = recurr(jump-stones.begin(),lastJump,stones);
+            ans3 = recurr(jump-stones.begin(),i,stones);
         }
         if(jump_plus < stones.end() && (*jump_plus -stones[i]) == lastJump + 1){
-            ans1 = recurr(jump_plus-stones.begin(),lastJump+1,stones);
+            ans1 = recurr(jump_plus-stones.begin(),i,stones);
         }
                          
         if(jump_min < stones.end() && (*jump_min - stones[i]) == lastJump - 1){
-             ans2 = recurr(jump_min-stones.begin(),lastJump-1,stones);
+             ans2 = recurr(jump_min-stones.begin(),i,stones);
         }
-        return dp[{i,lastJump}] = (ans1|ans2|ans3);
+        return dp[{i,j}] = (ans1|ans2|ans3);
     }
     bool canCross(vector<int>& stones) {
         return recurr(0,0,stones);
