e301a7f394dc339ab4223e357d77c2e32bec615d
Shreyas Pandey <49951762+shreydevep@users.noreply.github.com>
2022-07-15 15:05:17 +0530

    Time: 229 ms (97.82%), Space: 50.3 MB (68.07%) - LeetHub

---
+++ 493-reverse-pairs/493-reverse-pairs.cpp
@@ -0,0 +1,63 @@
+class Solution {
+    class MergeSort {
+
+    public:
+	    vector<int> arr;
+	    vector<int> temp;
+        int ans;
+	MergeSort(vector<int> tmp) {
+		arr = tmp;
+		temp.resize(arr.size());
+        ans = 0;
+		
+	}
+	void printArr() {
+		for (auto &x : arr) {
+			cout << x << " ";
+		}
+		cout << "\n";
+	}
+	void merge(int left, int mid, int right) {
+		int i = left;
+        int j = mid+1;
+        int k = left;
+        int r = mid+1;
+        int counter = 0;
+        
+		while(i<=mid) {
+            
+            while(r<=right && (arr[i] > (long)2*arr[r])) counter++, r++;
+            ans+= counter;
+            while( j<=right && arr[j]<=arr[i] ) {
+                temp[k++] = arr[j++];
+            }
+            temp[k++] = arr[i++];
+        }
+        while(j<=right) {
+            temp[k++] = arr[j++];
+        }
+		for(i=left;i<=right;++i){
+			arr[i] = temp[i];
+		}
+       
+	}
+	void merge_sort(int i, int j) {
+		if (i < j) {
+			int mid = (i+j)/2;
+            
+			merge_sort(i, mid);
+			merge_sort(mid + 1, j);
+			merge(i, mid, j);
+           
+		}
+        
+	}
+};
+public:
+    int reversePairs(vector<int>& nums) {
+        MergeSort* ms = new MergeSort(nums);
+        ms->merge_sort(0,nums.size()-1);
+        return ms->ans;
+       
+    }
+};
