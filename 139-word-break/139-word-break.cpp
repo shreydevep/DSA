602fc321d5b472a243de656ffa3fb6f1ed5f679f
Shreyas Pandey <49951762+shreydevep@users.noreply.github.com>
2022-07-19 11:44:17 +0530

    Time: 266 ms (5.02%), Space: 14.5 MB (28.86%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -1,29 +1,24 @@
 class Solution {
 public:
-    set <string> dict;
-    bool isPresent(string s){
-        if(dict.find(s) == dict.end()) return 0;
-        return 1;
-    }
-    void mcm(int i,int j,string &s,vector<vector<bool>> &dp){    
-        dp[i][j] = (dp[i][j] || isPresent(s.substr(i,j-i+1)));
-        //cout << isPresent(s.substr(i,j-i+1)) <<" "<< s.substr(i,j-i+1) <<"\n";
-        for(int k=i;k<j;++k){
-            // break
-           // cout << i <<" "<< k <<" "<< k+1 <<" "<< j <<"\n";
-            dp[i][j] = (dp[i][j] || (dp[i][k] && dp[k+1][j]));
+    vector<vector<int>> dp;
+    bool isCheck(string temp,vector<string> &wordDict){
+        for(auto &str : wordDict){
+            if(str == temp) return true;
         }
+        return false;
+    }
+    int recurr(int i,int j,string &s,vector<string> &wordDict){
+        if(i > j) return 1;
+        if(dp[i][j] != -1) return dp[i][j];
+        bool isPossible = isCheck(s.substr(i,j-i+1),wordDict);
+        for(int k=i;k<j;++k){
+            bool temp = recurr(i,k,s,wordDict) && recurr(k+1,j,s,wordDict);
+            isPossible |= temp;
+        }
+        return dp[i][j] = isPossible;
     }
     bool wordBreak(string s, vector<string>& wordDict) {
-        for(auto x : wordDict) dict.insert(x);
-        vector<vector<bool>> dp(s.length()+1,vector<bool>(s.length()+1,0));
-        for(int len=0;len<s.length();++len){
-            for(int i=0;i<s.length()-len;++i){
-                int j = i + len;
-                //cout << i <<" "<< j <<"\n";
-                mcm(i,j,s,dp);
-            }
-        }
-        return dp[0][s.length()-1];
+        dp.resize(s.length(),vector<int>(s.length(),-1));
+        return recurr(0,s.length()-1,s,wordDict);
     }
 };
