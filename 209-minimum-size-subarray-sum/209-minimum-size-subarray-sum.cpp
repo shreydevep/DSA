f4e6f58692e9688f61987208c4a197174d627713
Shreyas Pandey <49951762+shreydevep@users.noreply.github.com>
2022-07-22 22:28:16 +0530

    Time: 85 ms (9.96%), Space: 28.3 MB (20.91%) - LeetHub

--- 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
@@ -1,37 +1,19 @@
 class Solution {
 public:
-    
-    int minSubArrayLen(int k, vector<int>& nums) {
-        int i=0;
-        deque<int> q;
-        int ans = INT_MAX;
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int i = 0;
         int sum = 0;
-        while(i < nums.size() && nums[i] < 0) i++;
-        while(i < nums.size()){
-            
-            if(sum + nums[i] >= k){
-                ans = min(ans, (int)q.size()+1);
-                while(q.size() > 0 && sum >= k){
-                        sum -= q.front();
-                        ans = min(ans, (int)q.size());
-                        q.pop_front();
-                }
+        int ans = INT_MAX;
+        for(int j=0;j<nums.size();++j){
+            sum += nums[j];
+            while(i <= j && sum >= target){
+                
+                ans = min(ans,j-i+1);
+                sum -= nums[i];
+                i++;
             }
-            sum += nums[i];
-            q.push_back(nums[i]);
-            i++;
         }
-        while(!q.empty()){
-            if(sum >= k){
-                ans = min(ans,(int)q.size());
-            }
-            sum -= q.front();
-            q.pop_front();
-        }
-        if(sum >= k){
-            ans = min(ans,(int)q.size());
-        }
-        if(ans == INT_MAX) ans = 0;
-        return ans;
+        
+        return (ans != INT_MAX) ? ans : 0;
     }
 };
