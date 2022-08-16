5db9f1c6826ff6161ecb4aa86af452565e8092a6
Shreyas Pandey <49951762+shreydevep@users.noreply.github.com>
2022-08-16 12:38:29 +0530

    Time: 94 ms (60.93%), Space: 6.8 MB (97.33%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,42 +1,66 @@
 class Solution {
 public:
-     string recurr(int i,int j,string s){
-        
-        vector<vector<bool>> dp(s.length(),vector<bool> (s.length(),0));
-        int cnt = 0;
+   string expandAroundCenter(string &s){
+        int ans = 1;
+        pair<int,int> res = {0,0};
         for(int i=0;i<s.length();++i){
-            dp[i][i] = 1;
-        }
-        cnt++;
-        for(int i=0;i<s.length()-cnt;++i){
-            dp[i][i+1] = (s[i] == s[i+1]);
-        }
-        cnt++;
-        for(int dig=2;dig<s.length();++dig){
-            for(int i=0;i<s.length()-cnt;++i){
-                int j = i + dig;
+            cout << ans <<"\n";
+            if(i > 0){
+                //Even
+                int left = i-2;
                 
-                dp[i][j] = dp[i+1][j-1] and (s[i] == s[j]);
-            }
-            cnt++;
-        }
-        int ans = 0;
-         pair<int,int> temp  = {0,0};
-        for(int i=0;i<s.length();++i){
-            for(int j=i;j<s.length();++j){
-                if(dp[i][j]){
-                   if(ans < (j-i+1)){
-                       temp = {i,j};
-                       ans = max(ans,j-i+1);
-                   }
+                int right = i+1;
+                
+                if(s[i-1] == s[i]){
+                    
+                    if(ans < 2){
+                        ans = 2;
+                        res = {i-1,i};
+                    }
+                    
+                    while(left >= 0 && right < s.length()){
+                        if(s[left] == s[right]){
+                            if(ans < (right-left+1)){
+                                ans = right - left + 1;
+                                res = {left,right};
+                            }
+                            left--;
+                            right++;
+                        }
+                        else{
+                            break;
+                        }
+                    }
                 }
+                
+                
+                left = i-1;
+                right = i+1;
+                
+                while(left >= 0 && right < s.length()){
+                        if(s[left] == s[right]){
+                          
+                             if(ans < (right-left+1)){
+                                 
+                                 ans = right - left + 1;
+                                res = {left,right};
+                            }
+                            left--;
+                            right++;
+                        }
+                        else{
+                            break;
+                        }
+                }
+                
+                
+                //Odd
             }
+            
         }
-         return s.substr(temp.first,temp.second-temp.first+1);
-        
-        
+        return s.substr(res.first,res.second-res.first+1);
     }
     string longestPalindrome(string s) {
-        return recurr(0,s.length()-1,s);  
+       return expandAroundCenter(s);
     }
 };
